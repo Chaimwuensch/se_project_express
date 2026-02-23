ab488a280490160affe75e317bcb1819ca12d8f1
chaimwuensch <chaimwuensch@Gmail.com>
2026-02-23 14:12:46 -0500

    eslint

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-
-
 const clothingController = require('../controllers/clothingItems');
 
 router.get('/items', clothingController.getClothingItems);
