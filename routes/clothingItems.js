9cedaff37049d79dacd0fdc17552d0fd346ef357
chaimwuensch <chaimwuensch@Gmail.com>
2026-02-23 14:10:56 -0500

    exact eslint require spacing

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
+
+
 const clothingController = require('../controllers/clothingItems');
 
 router.get('/items', clothingController.getClothingItems);
